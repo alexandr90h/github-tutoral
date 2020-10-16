37ab91d5b9f824ec403455c52be897d4d3be3647
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2020-10-16 17:26:03 +0300

    16.10.2020-1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 const roundBoxElem = document.querySelector('.round-box');
-roundBoxElem;
+const arrowElem = document.querySelector('arrow-box');
+const btnChangeElem = document.querySelector('.change-time-btn');
+btnChangeElem.addEventListener('click', arrowMove);
+function arrowMove() {
+    // let deg = 0;
+    // arrowElem.style.transform=`rotate3d(0, 0, 1, 10deg)`;
+    console.log('gbntu');
+}
