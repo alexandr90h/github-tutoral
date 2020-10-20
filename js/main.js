1883a6f0f9b757e0534a57faa12f4e11c61fecec
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2020-10-20 16:38:36 +0300

    20.10.2020-1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 const roundBoxElem = document.querySelector('.round-box');
 const arrowElem = document.querySelector('.arrow-box');
-const btnChangeElem = document.querySelector('.change-time-btn');
-btnChangeElem.addEventListener('click', arrowMove);
-    arrowElem.style.transform=`rotate3d(0, 0, 1, 0deg)`;
+const btnStartElem = document.querySelector('.change-time-btn');
+const btnStopElem = document.querySelector('.stop-btn');
+btnStartElem.addEventListener('click', arrowMove);
+btnStopElem.addEventListener('click', arrowStop);
+// arrowElem.style.transform = `rotate3d(0, 0, 1, 0deg)`;
+let deg = 0;
+let timerId = null;
 
 function arrowMove() {
-    let deg = window.getComputedStyle(arrowElem).transform;
-    console.log(deg);
-    // arrowElem.style.transform=`rotate3d(0, 0, 1, ${deg++}deg)`;
-    // console.log('gbntu');
+    // let deg = window.getComputedStyle(arrowElem).transform;
+    // console.log(deg);
+    timerId = setInterval(() => {
+        arrowElem.style.transform = `rotate3d(0, 0, 1, ${deg+=6}deg)`;
+        console.log(timerId);
+    }, 1000)
+}
+function arrowStop() {
+    clearInterval(timerId);
+    console.log(timerId);
+    deg = -6;
+    arrowElem.style.transform = `rotate3d(0, 0, 1, ${deg+=6}deg)`;
 }
